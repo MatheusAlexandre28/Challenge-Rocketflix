80183e2f72ad20f19d2273dc21ca2556a771bbe1
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-02 17:28:57 -0300

    Consumindo API no front e no back

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  return res.json([
-    { name: 'jeff'},
-    { name: 'diego'}
-  ])
+app.get('/', async(req, res) => {
+
+  try {
+    //  response é a resposta do axios Mas eu tiro o data de dentro do response
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+    console.log(data)
+  
+    return res.json(data)
+    
+  } catch (error) {
+    console.log(error)
+  }
+
 })
 app.listen('4567')
