ec5ceee912d757a565427afba83e0a5c1b25a78d
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-02 16:17:44 -0300

    Todo o HTML feito e agr estudando o JS
     Estou aprendendo a fazer um API

---
+++ main.js
@@ -0,0 +1,10 @@
+import {
+  API_KEY, BASE_URL,
+  IMG_URL,
+  language,
+} from './api.js'
+
+// export const API_KEY = 'e8c7db52a2a42d93a081a9ca14524b68';  
+// export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+// export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+// export const language = 'language=pt-BR';
