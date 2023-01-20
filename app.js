7e6b9e4f343030be4a30e4f85307318b6956cf24
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-20 16:11:18 -0300

    API_key colocada no site que a rocket passou

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const movieTitle =  document.querySelector('.movie-title')
 const movieDescription =  document.querySelector('.movie-Description')
 const getRandomMovieButton = document.querySelector('.find-movie')
 
+const API_KEY = 'e8c7db52a2a42d93a081a9ca14524b68'
+
 getRandomMovieButton.addEventListener('click', async () => {
   const movie = await fetch('https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}')
 })
