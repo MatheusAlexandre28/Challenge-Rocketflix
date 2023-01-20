fd7a10130deb90e80080639069ecbf4a735c1bb4
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-20 16:12:51 -0300

    json
    Estrutura de dados que conseguimos manipular muito facil com JS

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ const API_KEY = 'e8c7db52a2a42d93a081a9ca14524b68'
 
 getRandomMovieButton.addEventListener('click', async () => {
   const movie = await fetch('https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}')
+  const movieData = await movie.json()
+
 })
