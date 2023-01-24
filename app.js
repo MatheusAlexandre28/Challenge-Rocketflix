dbdf27ce154663a1f0eebbf4c91bf3a0433d7de5
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-24 16:03:40 -0300

    atribumos uma ação ao button e um fetch

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const movieContainer = document.querySelector('.movie-info')
 const moviePoster =  document.querySelector('.movie-poster')
 const movieTitle =  document.querySelector('.movie-title')
 const movieDescription =  document.querySelector('.movie-Description')
-const getRandomMovieButton = document.querySelector('.get-random-movie')
+const getRandomMovieButton = document.querySelector('.find-movie')
+
+getRandomMovieButton.addEventListener('click', () => {
+  const movie = fetch
+})
