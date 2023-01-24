a637bac909c70a762856f0ff442381fbf63436f3
Matheus <69991941+MatheusAlexandre28@users.noreply.github.com>
2023-01-24 15:58:20 -0300

    Peguei todos os selectors

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-const movieContainer = document.querySelector('.main')
+const movieContainer = document.querySelector('.movie-info')
+const moviePoster =  document.querySelector('.movie-poster')
+const movieTitle =  document.querySelector('.movie-title')
+const movieDescription =  document.querySelector('.movie-Description')
+const getRandomMovieButton = document.querySelector('.get-random-movie')
